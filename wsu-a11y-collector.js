f4dcb42bf94a0a7a426692038b5c1517a97f2416
Jeremy Felt <jeremy.felt@gmail.com>
2017-02-28 17:47:49 -0800

    Accept URL as an argument rather than a static variable

--- wsu-a11y-collector.js
+++ wsu-a11y-collector.js
@@ -1,11 +1,15 @@
 "use strict";
 
+if ( process.argv.length < 3 ) {
+	console.log( "Please specify a URL to scan." );
+	process.exit();
+}
+
 var collector = {};
 var pa11y = require( "pa11y" );
 var elasticsearch = require( "elasticsearch" );
-
-var url = "https://wsu.edu";
-var domain = "wsu.edu";
+var parse_url = require( "url" );
+var url = parse_url.parse( process.argv[ 2 ] );
 
 collector.collect = pa11y( {
 	standard: "WCAG2AA",
@@ -25,15 +29,15 @@ collector.elastic = new elasticsearch.Client( {
 // Delete any previous records stored for this URL.
 collector.elastic.deleteByQuery( {
 	index: "a11y-scan",
-    q: "url:" + encodeURIComponent( url ),
+    q: "url:" + encodeURIComponent( url.href ),
 	body: {}
 }, function( error, response ) {
 	if ( undefined !== typeof response ) {
-		console.log( "Deleted " + response.total + " previous records for " + url + " in " + response.took + " ms." );
+		console.log( "Deleted " + response.total + " previous records for " + url.href + " in " + response.took + " ms." );
 	}
 } );
 
-collector.collect.run( url, function( error, result ) {
+collector.collect.run( url.href, function( error, result ) {
 	if ( error ) {
 		return console.error( error.message );
 	}
@@ -43,8 +47,8 @@ collector.collect.run( url, function( error, result ) {
 	// Append domain and URL information to each result and build a
 	// set of bulk data to send to ES.
 	for ( var i = 0, x = result.length; i < x; i++ ) {
-		result[ i ].domain = domain;
-		result[ i ].url = url;
+		result[ i ].domain = url.hostname;
+		result[ i ].url = url.href;
 
 		bulk_body.push( { index: { _index: "a11y-scan", _type: "scan-record" } } );
 		bulk_body.push( result[ i ] );
@@ -54,7 +58,7 @@ collector.collect.run( url, function( error, result ) {
 		body: bulk_body
 	}, function( err, response ) {
 		if ( undefined !== typeof response ) {
-			console.log( "Accessibility scan on " + url + " took " + response.took + "ms and logged " + response.items.length + " records." );
+			console.log( "Accessibility scan on " + url.href + " took " + response.took + "ms and logged " + response.items.length + " records." );
 		} else {
 			console.log( err );
 		}
